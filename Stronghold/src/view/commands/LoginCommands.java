bdb9bbb34d17aa8ce64aa8a36bcb66b23a43da06
motrnam <motrnam@gmail.com>
2023-04-16 08:58:22 +0330

    login menu command added

--- Stronghold/src/view/commands/LoginCommands.java
+++ Stronghold/src/view/commands/LoginCommands.java
@@ -1,4 +1,19 @@
 package view.commands;
 
 public enum LoginCommands {
+    LOGIN("\\s*login\\s+-u\\s+(?<username>\\S+)\\s+-p\\s+(?<password>\\S+)(\\s+--stay-logged-in)?\\s*"),
+    BACK("\\s*back\\s*"),
+    HELP("\\s*help\\s*"),
+    FORGET_PASSWORD("\\s*forget\\s+password\\s*"),
+    FORGET_USERNAME("\\s*forget\\s+username");
+    public String regex;
+    LoginCommands(String regex) {
+        this.regex = regex;
+    }
+    public static Matcher getMatcher(String input, MemberMenuCommands mainRegex) {
+        Matcher matcher = Pattern.compile(mainRegex.regex).matcher(input);
+        if (matcher.matches())
+            return matcher;
+        return null;
+    }
 }
--- Stronghold/src/view/commands/TradeCommand.java
+++ Stronghold/src/view/commands/TradeCommand.java
@@ -1,4 +1,5 @@
 package view.commands;
 
 public enum TradeCommand {
+
 }
