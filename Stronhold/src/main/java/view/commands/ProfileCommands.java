e13e3d8244f796995e6f2db7867797df062208b3
motrnam <motrnam@gmail.com>
2023-04-29 09:03:15 +0330

    some changes

--- Stronhold/src/main/java/view/commands/ProfileCommands.java
+++ Stronhold/src/main/java/view/commands/ProfileCommands.java
@@ -4,8 +4,8 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public enum ProfileCommands {
-    CHANGE_SOMETHING("\\s*change\\s+(?<whichOption>\\S+)\\s+(?<newOption>\\S+)\\s*"),
-    CHANGE_PASSWORD("\\s*change\\s+password\\s+-o\\s+(?<oldPassword>\\S+)\\s+-n\\s+(?<newPassword>\\S+)\\s+-c\\s+(?<confirm>\\S+)\\s*"),
+    CHANGE_SOMETHING("\\s*change\\s+(?<whichOption>\\S+)\\s+(?<newOption>(\\S+|\"[^\"]\"))\\s*"),
+    CHANGE_PASSWORD("\\s*change\\s+password\\s+-o\\s+(?<oldPassword>(\\S+|\"[^\"]\"))\\s+-n\\s+(?<newPassword>(\\S+|\"[^\"]\"))\\s+-c\\s+(?<confirm>(\\S+|\"[^\"]\"))\\s*"),
     LOGOUT("\\s*logout\\s*"),
     DISPLAY_PROFILE("\\s*display\\s*(?<part>.*)\\s*"),
     SHOW_MAP("\\s*show\\s+map\\s+-x\\s+(?<coordinateX>\\d+)\\s+-y\\s+(?<coordinateY>\\d+)\\s*"),
