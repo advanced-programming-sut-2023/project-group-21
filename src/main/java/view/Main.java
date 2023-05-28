2d538701770745d4e9f8155207d9bf33c62c2b96
Sobhan Lotfi <sobhan.lotfi121@gmail.com>
2023-05-28 23:03:52 +0430

    starting menu: stage configuration, background and 2 buttons

---
+++ src/main/java/view/Main.java
@@ -0,0 +1,16 @@
+package view;
+
+import controller.FileController;
+import model.User;
+import view.PlayerMenu;
+import view.StartMenu;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        StartingMenu.main(args);
+    }
+}
+
+// login -u shayaaaan -p n₨Oqm.U0Q19hb --stay-logged-in
