295b3eca825b9daa1ae66b309936d1d9f18944fd
motrnam <motrnam@gmail.com>
2023-06-28 21:53:23 +0330

    send Message added

--- src/main/java/model/MessageGui.java
+++ src/main/java/model/MessageGui.java
@@ -3,7 +3,6 @@ package model;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
