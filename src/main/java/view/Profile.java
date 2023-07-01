6e283c2512cc19fd4e03f00e89c8f9ab20953497
Sobhan Lotfi <sobhan.lotfi121@gmail.com>
2023-07-02 00:14:51 +0330

    leaderboard being online

--- src/main/java/view/Profile.java
+++ src/main/java/view/Profile.java
@@ -81,9 +81,9 @@ public class Profile extends Application {
             table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 
 //            FileController.getAllUsers().sort(User::compareTo);
-//            ObservableList<User> users= FXCollections.observableArrayList(FileController.getAllUsers());
+            ObservableList<User> users= FXCollections.observableArrayList(FileController.getAllUsers());
 
-            //table.setItems(users);
+            table.setItems(users);
             scoreColumn.setSortType(TableColumn.SortType.DESCENDING);
             table.getSortOrder().add(scoreColumn);
             table.prefHeightProperty().bind(boardStage.heightProperty());
